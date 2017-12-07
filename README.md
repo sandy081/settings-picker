ee9a4db3a68a537d8c2afd860f3079456def2761
Sandeep Somavarapu <sasomava@microsoft.com>
2017-12-07 23:45:02 +0100

    New commands

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ A VS Code extension for easily picking a setting
 ## Features
 
 * `Toggle Setting` command will toggle the selected setting of type `boolean`
+* `Update Setting` command will allow to select and update a setting.
+* `Reset Setting` command will reset the selected setting to default value.
--- src/extension.ts
+++ src/extension.ts
@@ -2,6 +2,7 @@
 import * as vscode from 'vscode';
 import { IJSONSchema } from './jsonSchema';
 import { STATUS_CODES } from 'http';
+import { ConfigurationTarget } from 'vscode';
 
 export async function activate(context: vscode.ExtensionContext) {
 
@@ -17,6 +18,8 @@ export async function activate(context: vscode.ExtensionContext) {
 	});
 
 	vscode.commands.registerCommand('setting.toggle', () => configuration.toggle());
+	vscode.commands.registerCommand('setting.update', () => configuration.pickAndUpdate());
+	vscode.commands.registerCommand('setting.reset', () => configuration.reset());
 }
 
 export function deactivate() {
@@ -31,7 +34,7 @@ export class Configuration {
 		this.recentlyUpdated = JSON.parse(this.context.globalState.get('settings.picker.recentlyUpdated', '[]'));
 	}
 
-	public update(defaultConfigurations: IJSONSchema) {
+	update(defaultConfigurations: IJSONSchema) {
 		const register = (configuration: IJSONSchema) => {
 			let properties = configuration.properties;
 			if (properties) {
@@ -54,52 +57,199 @@ export class Configuration {
 		register(defaultConfigurations);
 	}
 
-	toggle(): void {
-		vscode.window.showQuickPick(this.getBooleanSettings(), { matchOnDescription: true, placeHolder: "Select the setting to toggle" })
-			.then(item => this.toggleSetting(item.label));
+	async toggle() {
+		const item = await vscode.window.showQuickPick(this.getBooleanSettings(), { matchOnDescription: true, placeHolder: "Select the setting to toggle" });
+		if (item) {
+			await this.toggleSetting(item.label);
+		}
+	}
+
+	async pickAndUpdate() {
+		const item = await vscode.window.showQuickPick(this.getSettings(), { matchOnDescription: true, placeHolder: "Select the setting to update" });
+		if (item) {
+			const setting = item.label;
+			const schema = this.settings.get(item.label);
+			if (schema) {
+				if (schema.enum && schema.enum.length) {
+					await this.updateEnumTypeSetting(setting, schema);
+				} else {
+					switch (schema.type) {
+						case 'string':
+							await this.updateStringTypeSetting(setting, schema);
+							break;
+						case 'number':
+							await this.updateNumberTypeSetting(setting, schema);
+							break;
+						case 'boolean':
+							await this.updateBooleanTypeSetting(setting, schema);
+							break;
+					}
+				}
+			}
+		}
+	}
+
+	async reset() {
+		const item = await vscode.window.showQuickPick(this.getSettings(), { matchOnDescription: true, placeHolder: "Select the setting to update" });
+		if (item) {
+			const configuration = vscode.workspace.getConfiguration();
+			const setting = item.label;
+			const inspect = configuration.inspect(setting);
+			if (inspect) {
+				if (inspect.workspaceValue !== undefined) {
+					await this.updateSetting(configuration, setting, void 0, ConfigurationTarget.Workspace);
+				}
+				if (inspect.globalValue !== undefined) {
+					await this.updateSetting(configuration, setting, void 0, ConfigurationTarget.Global);
+				}
+			}
+		}
+	}
+
+	private async updateEnumTypeSetting(setting: string, schema: IJSONSchema) {
+		const configuration = vscode.workspace.getConfiguration();
+		const currentValue = configuration.get(setting);
+		const inspect = configuration.inspect(setting);
+		if (schema.enum && schema.enum.length) {
+			const item = await vscode.window.showQuickPick(schema.enum.map((e, index) => (<vscode.QuickPickItem>{
+				label: e,
+				description: `${currentValue === e ? '(Current)' : ''}${inspect.defaultValue === e ? '(Default)' : ''}`
+			})));
+			if (item && currentValue !== item.label) {
+				await this.updateSetting(configuration, setting, item.label);
+			}
+		}
+	}
+
+	private async updateBooleanTypeSetting(setting: string, schema: IJSONSchema) {
+		const configuration = vscode.workspace.getConfiguration();
+		const currentValue = configuration.get(setting);
+		const inspect = configuration.inspect(setting);
+		const item = await vscode.window.showQuickPick([
+			{
+				label: 'true',
+				description: `${currentValue === true ? '(Current)' : ''}${inspect.defaultValue === true ? '(Default)' : ''}`
+			},
+			{
+				label: 'false',
+				description: `${currentValue === false ? '(Current)' : ''}${inspect.defaultValue === false ? '(Default)' : ''}`
+			}
+		]);
+		if (item) {
+			const value = item.label === 'true';
+			if (value !== currentValue) {
+				await this.updateSetting(configuration, setting, value);
+			}
+
+		}
+	}
+
+	private async updateStringTypeSetting(setting: string, schema: IJSONSchema) {
+		const configuration = vscode.workspace.getConfiguration();
+		const currentValue = configuration.get(setting);
+		const inspect = configuration.inspect(setting);
+		const value = await vscode.window.showInputBox({
+			placeHolder: `Provide value for setting ${setting}`,
+			prompt: `Current value: ${currentValue}`,
+		});
+		if (value && currentValue !== value) {
+			await this.updateSetting(configuration, setting, value);
+		}
+	}
+
+	private async updateNumberTypeSetting(setting: string, schema: IJSONSchema) {
+		const configuration = vscode.workspace.getConfiguration();
+		const currentValue = configuration.get(setting);
+		const inspect = configuration.inspect(setting);
+		const inputValue = await vscode.window.showInputBox({
+			placeHolder: `Provide value for setting ${setting}`,
+			prompt: `Current value: ${currentValue}`,
+			validateInput: inputValue => {
+				try {
+					parseInt(inputValue)
+				} catch (e) {
+					return 'Provide a number value';
+				}
+				return null;
+			}
+		});
+		if (inputValue) {
+			const value = parseInt(inputValue);
+			if (currentValue !== value) {
+				await this.updateSetting(configuration, setting, value);
+			}
+		}
 	}
 
 	private getBooleanSettings(): vscode.QuickPickItem[] {
 		const booleanSettings: vscode.QuickPickItem[] = [];
+		const configuration = vscode.workspace.getConfiguration();
 
 		for (const setting of this.recentlyUpdated) {
 			const schema = this.settings.get(setting);
 			if (schema && schema.type === 'boolean') {
+				const value = configuration.get(setting);
 				booleanSettings.push({
 					label: setting,
-					description: schema.description
+					description: `${value}≫${!value}­­	☛${schema.description}`
 				});
 			}
 		}
 
 		this.settings.forEach((schema, setting) => {
 			if (this.recentlyUpdated.indexOf(setting) === -1 && schema.type === 'boolean') {
+				const value = configuration.get(setting);
 				booleanSettings.push({
 					label: setting,
-					description: schema.description
+					description: `${value}≫${!value}­­	☛${schema.description}`
 				});
 			}
 		});
 		return booleanSettings;
 	}
 
-	private async toggleSetting(setting: string) {
-		const configuration = vscode.workspace.getConfiguration();
-		const inspect = configuration.inspect(setting);
-		const value = configuration.get(setting)
-		if (inspect) {
-			await this.updateSetting(configuration, setting, !value, inspect.workspaceValue !== void 0 ? vscode.ConfigurationTarget.Workspace : vscode.ConfigurationTarget.Global);
+	private getSettings(): vscode.QuickPickItem[] {
+		const settings: vscode.QuickPickItem[] = [];
+
+		for (const setting of this.recentlyUpdated) {
+			const schema = this.settings.get(setting);
+			if (schema) {
+				settings.push({
+					label: setting,
+					description: `☛${schema.description}`
+				});
+			}
 		}
+
+		this.settings.forEach((schema, setting) => {
+			if (this.recentlyUpdated.indexOf(setting) === -1) {
+				settings.push({
+					label: setting,
+					description: `☛${schema.description}`
+				});
+			}
+		});
+		return settings;
 	}
 
-	private async updateSetting(configuration: vscode.WorkspaceConfiguration, setting: string, value: any, target: vscode.ConfigurationTarget) {
-		await configuration.update(setting, value, target);
-		const index = this.recentlyUpdated.indexOf(setting);
-		if (index !== -1) {
-			this.recentlyUpdated.splice(index, 1);
+	private async toggleSetting(setting: string) {
+		const configuration = vscode.workspace.getConfiguration();
+		const value = configuration.get(setting)
+		await this.updateSetting(configuration, setting, !value);
+	}
+
+	private async updateSetting(configuration: vscode.WorkspaceConfiguration, setting: string, value: any, target?: vscode.ConfigurationTarget) {
+		const inspect = configuration.inspect(setting);
+		if (inspect) {
+			target = target !== void 0 ? target : inspect.workspaceValue !== void 0 ? vscode.ConfigurationTarget.Workspace : vscode.ConfigurationTarget.Global;
+			await configuration.update(setting, value, target);
+			const index = this.recentlyUpdated.indexOf(setting);
+			if (index !== -1) {
+				this.recentlyUpdated.splice(index, 1);
+			}
+			this.recentlyUpdated.splice(0, 0, setting);
+			this.context.globalState.update('settings.picker.recentlyUpdated', JSON.stringify(this.recentlyUpdated));
 		}
-		this.recentlyUpdated.splice(0, 0, setting);
-		this.context.globalState.update('settings.picker.recentlyUpdated', JSON.stringify(this.recentlyUpdated));
 	}
 
 }
